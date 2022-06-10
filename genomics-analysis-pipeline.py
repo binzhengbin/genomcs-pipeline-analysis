741ce0478650f2325edb266912bf7a279757ed96
binzheng <87288334+binzhengbin@users.noreply.github.com>
2022-06-10 21:51:16 +0800

    Add files via upload

--- genomics-analysis-pipeline.py
+++ genomics-analysis-pipeline.py
@@ -4,32 +4,35 @@
 # genomics analysis pipeline python script
 #############################################################################
 
-REP_INDEX = {"007","012","013","031","043","066","071","072","080","084"}
-{INDEX_BWA}
+REP_INDEX = {"HS007","HS012","HS013","HS031","HS043","HS066","HS071","HS072","HS080","HS084", \
+             "HT011","HT027","HT032","HT044","HT047","HT055","HT056","HT061","HT068","HT077", \
+             "HT081","HT089"}
+{INDEX_BWA} = "/home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/galGal6a/"
+
 
 rule all:
     input:
-        expand("fastpresult/HS_afqc_{rep}_1.fq",rep=REP_INDEX),
-        expand("fastpresult/HS_afqc_{rep}_2.fq",rep=REP_INDEX),
-        expand("fastpresult/HS_unpaired_{rep}_1.fq",rep=REP_INDEX),
-        expand("fastpresult/HS_unpaired_{rep}_2.fq",rep=REP_INDEX),
-        expand("fastpresult/HS_fastp_{rep}.html",rep=REP_INDEX),
-        expand("fastpresult/HS_fastp_{rep}.json",rep=REP_INDEX),
-        expand("fastqcresult/HS_{rep}_fastp.log",rep=REP_INDEX)
+        expand("fastpresult/afqc_{rep}_1.fq",rep=REP_INDEX),
+        expand("fastpresult/afqc_{rep}_2.fq",rep=REP_INDEX),
+        expand("fastpresult/unpaired_{rep}_1.fq",rep=REP_INDEX),
+        expand("fastpresult/unpaired_{rep}_2.fq",rep=REP_INDEX),
+        expand("fastpresult/fastp_{rep}.html",rep=REP_INDEX),
+        expand("fastpresult/fastp_{rep}.json",rep=REP_INDEX),
+        expand("fastqcresult/{rep}_fastp.log",rep=REP_INDEX)
 rule fastp:
     input:
-          "RawData/HS_{rep}_1.fq.gz",
-          "RawData/HS_{rep}_2.fq.gz"
+          "RawData/{rep}_1.fq.gz",
+          "RawData/{rep}_2.fq.gz"
 
     output:
-          "fastpresult/HS_afqc_{rep}_1.fq",
-          "fastpresult/HS_afqc_{rep}_2.fq",
-          "fastpresult/HS_unpaired_{rep}_1.fq",
-          "fastpresult/HS_unpaired_{rep}_2.fq",
-          "fastpresult/HS_fastp_{rep}.html",
-          "fastpresult/HS_fastp_{rep}.json"
+          "1-fastpresult/afqc_{rep}_1.fq",
+          "1-fastpresult/afqc_{rep}_2.fq",
+          "1-fastpresult/unpaired_{rep}_1.fq",
+          "1-fastpresult/unpaired_{rep}_2.fq",
+          "1-fastpresult/fastp_{rep}.html",
+          "1-fastpresult/fastp_{rep}.json"
     log:
-          "fastqcresult/HS_{rep}_fastp.log"
+          "1-fastqcresult/{rep}_fastp.log"
     shell:
           "fastp --thread 16 --n_base_limit 15 \
           -h {output[4]} -j {output[5]} \
@@ -43,14 +46,14 @@ rule fastp:
 
 rule bwa:
     input:
-        "fastpresult/HS_afqc_{rep}_1.fq",
-        "fastpresult/HS_afqc_{rep}_2.fq"
+        "1-fastpresult/afqc_{rep}_1.fq",
+        "1-fastpresult/afqc_{rep}_2.fq"
     output:
-        "bwa_mapping/HS_afqc_{rep}_bwa_gal6a.bam"
+        "2-bwa_mapping/afqc_{rep}_bwa_gal6a.bam"
     params:
         "@RG\tID:{rep}\tSM:{rep}\tPL:illumina"
     log:
-        "bwa_mapping/HS_afqc_{rep}_bwa_gal6a.log"
+        "2-bwa_mapping/afqc_{rep}_bwa_gal6a.log"
     shell:
         "bwa mem {INDEX_BWA} -t 12 \
         -R {params} \
@@ -60,31 +63,58 @@ rule bwa:
 
 rule bam_file_sort:
     input:
-        "bwa_mapping/HS_afqc_{rep}_bwa_gal6a.bam"
+        "2-bwa_mapping/afqc_{rep}_bwa_gal6a.bam"
     output:
-        "bwa_sorted/HS_afqc_{rep}_bwa_sorted_gal6a.bam"
+        "3-bwa_sorted/afqc_{rep}_bwa_sorted_gal6a.bam"
     log:
-        "bwa_sorted/HS_afqc_{rep}_bwa_sorted_gal6a.log"
+        "3-bwa_sorted/afqc_{rep}_bwa_sorted_gal6a.log"
     shell:
         "samtolls sort -@ 4 -m 4G -O bam -o {output} {input}"
 
 rule remove_dumplication:
     input:
-        "bwa_sorted/HS_afqc_{rep}_bwa_sorted_gal6a.bam"
+        "3-bwa_sorted/afqc_{rep}_bwa_sorted_gal6a.bam"
     output:
-        "rmdup/HS_afqc_{rep}_bwa_sorted_rmdup_gal6a.bam"
-        "rmdup/HS_afqc_{rep}_bwa_sorted_rmdup_gal6a_metrics.txt"
-    params:
-        aso = r"queryname"
-        so = r"coordinate"
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam"
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a_metrics.txt"
     log:
-        "rmdup/HS_afqc_{rep}_bwa_sorted_rmdup_gal6a.log"
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.log"
     shell:
         "gatk MarkDuplicates \
         --INPUT {input} \
         --OUTPUT {output[0]} \
         --METRICS_FILE {output[1]} \
         --VALIDATION_STRINGENCY SILENT \
-        --ASSUME_SORT_ORDER '{params.aso}' \
         --CREATE_MD5_FILE false \
         --REMOVE_DUPLICATES false \ "
+
+rule Base_Quality_Score_Recalibration:
+    input:
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam"
+    output:
+        "5-baserecalibrator/{rep}_bqsr_data.table"
+    log:
+        "5-baserecalibrator/{rep}_bqsr_data.log"
+    shell:
+        "gatk BaseRecalibrator \
+         -R /home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/GCA_000002315.5_GRCg6a_genomic.fna \
+         -I {input} \
+         --use-original-qualities \
+         -O {output} \
+         --known-sites /home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/variation_gallus_gallus/gallus_gallus.20220314.vcf"
+
+rule ApplyBQSR:
+    input:
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam"
+        "5-baserecalibrator/{rep}_bqsr_data.table"
+    output:
+        "6-BQSR/{rep}_bqsr.bam"
+    log:
+        "6-BQSR/{rep}_bqsr.log"
+    shell:
+        "gatk ApplyBQSR \
+        -R /home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/GCA_000002315.5_GRCg6a_genomic.fna \
+        -I {input[0]} \
+        -O {output} \
+        -bqsr {input[1]} \
+        "
