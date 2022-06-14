082dc0b7a38f15c28eef990753dfc2d82831b6b8
binzheng <87288334+binzhengbin@users.noreply.github.com>
2022-06-14 17:12:16 +0800

    Add files via upload

--- genomics-analysis-pipeline.py
+++ genomics-analysis-pipeline.py
@@ -4,11 +4,16 @@
 # genomics analysis pipeline python script
 #############################################################################
 
+# sample ID
 REP_INDEX = {"HS007","HS012","HS013","HS031","HS043","HS066","HS071","HS072","HS080","HS084", \
              "HT011","HT027","HT032","HT044","HT047","HT055","HT056","HT061","HT068","HT077", \
              "HT081","HT089"}
+# bwa index pathway
 INDEX_BWA = "/home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/galGal6a/"
-
+# the reference SNP sites from dbsnp
+SNP_SITES = "/home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/variation_gallus_gallus/gallus_gallus.20220314.vcf"
+# reference genome pathway
+REFERENCE = "/home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/GCA_000002315.5_GRCg6a_genomic.fna "
 
 rule all:
     input:
@@ -33,7 +38,7 @@ rule all:
 
 rule fastp:
     input:
-        "RawData/{rep}_1.fq.gz",
+        "RawData/{rep}_1.fq.gz",\
         "RawData/{rep}_2.fq.gz"
 
     output:
@@ -48,7 +53,11 @@ rule fastp:
     shell:
         "fastp --thread 16 --n_base_limit 15 \
         -h {output[4]} -j {output[5]} \
-        --qualified_quality_phred 25 \
+        --trim_front1 3 \
+        --trim_front2 3 \
+        --adapter_sequence TCTTTCCCTACACGACGCTCTTCCGATCT \
+        --adapter_sequence_r2 AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC \
+        --qualified_quality_phred 20 \
         --unqualified_percent_limit 50 \
         -i {input[0]} -o {output[0]} \
         -I {input[1]} -O {output[1]} \
@@ -63,7 +72,7 @@ rule bwa:
     output:
         "2-bwa_mapping/afqc_{rep}_bwa_gal6a.bam"
     params:
-        "@RG\tID:{rep}\tSM:{rep}\tPL:illumina"
+        "@RG\tID:XinHua\tSM:{rep}\tPL:illumina"
     log:
         "2-bwa_mapping/afqc_{rep}_bwa_gal6a.log"
     shell:
@@ -88,17 +97,17 @@ rule remove_dumplication:
     input:
         "3-bwa_sorted/afqc_{rep}_bwa_sorted_gal6a.bam"
     output:
-        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam",
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam"
         "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a_metrics.txt"
     log:
         "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.log"
     shell:
         "gatk MarkDuplicates \
+        --java-options "-Xmx20G" \
+        --spark-master local[5] \
         --INPUT {input} \
         --OUTPUT {output[0]} \
         --METRICS_FILE {output[1]} \
-        --VALIDATION_STRINGENCY SILENT \
-        --CREATE_MD5_FILE false \
         --REMOVE_DUPLICATES false > {log} 2>&1 "
 
 rule Base_Quality_Score_Recalibration:
@@ -109,17 +118,18 @@ rule Base_Quality_Score_Recalibration:
     log:
         "5-baserecalibrator/{rep}_bqsr_data.log"
     shell:
-        "gatk BaseRecalibrator \
-        -R /home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/GCA_000002315.5_GRCg6a_genomic.fna \
+        "gatk BaseRecalibratorSpark \
+        --java-options "-Xmx20G" \
+        --spark-master local[5] \
+        -R {REFERENCE} \
         -I {input} \
-        --use-original-qualities \
         -O {output} \
-        --known-sites /home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/variation_gallus_gallus/gallus_gallus.20220314.vcf \
+        --known-sites {SNP_SITES} \
         > {log} 2>&1 "
 
 rule ApplyBQSR:
     input:
-        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam",
+        "4-rmdup/afqc_{rep}_bwa_sorted_markdup_gal6a.bam"
         "5-baserecalibrator/{rep}_bqsr_data.table"
     output:
         "6-BQSR/{rep}_bqsr.bam"
@@ -127,8 +137,25 @@ rule ApplyBQSR:
         "6-BQSR/{rep}_bqsr.log"
     shell:
         "gatk ApplyBQSR \
-        -R /home/bzheng/genomics-data/X101SC21110256-Z01-J031/ref/galGal6a/GCA_000002315.5_GRCg6a_genomic.fna \
+        --java-options "-Xmx20G" \
+        -R {REFERENCE} \
         -I {input[0]} \
         -O {output} \
         -bqsr {input[1]} \
+        > {log} 2>&1 "
+
+rule HaplotypeCaller:
+    input:
+        "6-BQSR/{rep}_bqsr.bam"
+    output:
+        "7-HaplotypeCaller/{rep}_SNP_only.GCVF.gz"
+    log:
+        "7-HaplotypeCaller/{rep}_SNP_only.log"
+    shell:
+        "gatk HaplotypeCaller \
+        --java-options "-Xmx20G" \
+        -R {REFERENCE} \
+        -ERC GVCF \
+        -I {input} \
+        -D {SNP_SITES} \
         > {log} 2>&1 "
